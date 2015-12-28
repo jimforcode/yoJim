a1a57097def5284ffb5acc3cc87ff42dd638c0df
zj <zj@xiaoxiao.la>
2015-12-28 17:45:36 +0800

    add event

--- app/scripts/business/base/InfoController.js
+++ app/scripts/business/base/InfoController.js
@@ -1,8 +1,28 @@
 define(function(require, exports, module) {
 	  require('styles/userDetail.css');
   console.log(" this is what i'm doing .....这是会加载的info.........");
-  exports.sayS = function(msg){
-    console.log(" well done ! "+msg);
-  }
+
+  var Controller = function(){
+    var controller = this;
+    controller.actions={
+      yo:function(){
+        alert(" 点嘛点!");
+      },
+      jim:function(){
+        alert(" 再点试试!");
+      },
+      jimi:function(){
+        alert("jimi was clicked!");
+      }
+    }
+
+  };
+
+
+
+  //exports.sayS = function(msg){
+  //  console.log(" well done ! "+msg);
+  //}
+  module.exports = Controller;
 
 });
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,23 +4,87 @@ define(function(require, exports, module) {
 	// var hash = decodeURI(window.location.hash);
 	// alert(hash);
 
-var author = function () { console.log("author"); };
-      var books = function () { console.log("books"); };
-      var viewBook = function (bookId) {
-        console.log("viewBook: bookId is populated: " + bookId);
-      };
+  //  var author = function () { console.log("author"); };
+  //    var books = function () { console.log("books"); };
+  //    var viewBook = function (bookId) {
+  //      console.log("viewBook: bookId is populated: " + bookId);
+  //    };
+  //var seajsLoad =function(){
+  //  ISeaJs.use('./scripts/business/base/InfoController');
+  //};
+  //
+  //    var routes = {
+  //      '/author': [author],
+  //      '/books': [books, seajsLoad,function() {
+  //        console.log("An inline route handler.");
+  //      }],
+  //      '/books/view/:bookId': [viewBook]
+  //    };
+  //
+  //    var router = Router(routes);
+  //
+  //    router.init();
+  var showAuthorInfo = function () { console.log("showAuthorInfo"); };
+  var listBooks = function () { console.log("listBooks"); };
 
-      var routes = {
-        '/author': [author],
-        '/books': [books, require("extend"),function() {
-          console.log("An inline route handler.");
-        }],
-        '/books/view/:bookId': [viewBook, ISeaJs.use('./scripts/business/base/InfoController')]
-      };
+  var allroutes = function() {
+    var route = window.location.hash.slice(2);
+    var sections = $('section');
+    var section;
 
-      var router = Router(routes);
+    section = sections.filter('[data-route=' + route + ']');
 
-      router.init();
+    if (section.length) {
+      sections.hide(250);
+      section.show(250);
+    }
+  };
+  var loadActions;
+
+  var seajsLoad =function(){
+    //return ISeaJs.use('./scripts/business/base/InfoController');
+    var  info=require('info');
+    var  _info = new info();
+    loadActions=_info.actions
+   };
+ //var action
+  //
+  // define the routing table.
+  //
+  var routes = {
+    '/author': showAuthorInfo,
+    '/books': listBooks,
+    '/seajsLoad':seajsLoad
+   };
+
+  //
+  // instantiate the router.
+  //
+  var router = Router(routes);
+
+  //
+  // a global configuration setting.
+  //
+  router.configure({
+    on: allroutes
+  });
+
+  router.init();
+
+  $(document).on("click", "[data-jim-action]", function(evt) {
+   alert(" clicked me!");
+    //console.log(evt = evt || window.event);
+
+
+    var _this = $(this),
+       actionName = _this.attr('data-jim-action');
+      var action = loadActions[actionName];
+    console.log(_this);
+    console.log(actionName);
+    console.log(action);
+    action && $.isFunction(action) && action.call(_this, evt);
+  });
+
 
 
 
